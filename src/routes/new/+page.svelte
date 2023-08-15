d2cccabf2b33000e7dd1b83876ac10535d9ef222
ccozens <ccozens@gmail.com>
2023-08-15 12:59:05 +0100

    this is the working setinterval demo

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -4,35 +4,24 @@
 	import Start from '$lib/components/Start.svelte';
 	import Stop from '$lib/components/Stop.svelte';
 
-	let interval;
+    let interval ;
 	// async function to start a timer
 	function testTimer() {
 		// abort if not started
-		if (!isStarted) return;
+        if (!isStarted) return;
 
-		while ($currentPeriod < 5) {
-			// if isRest, set interval to 1s
-			if (isRest) {
-				interval = setTimeout(() => {
-					console.log('rest');
-					isRest.set(false);
-					console.log('period: ', $currentPeriod);
-					if ($currentPeriod === 5) {
-						console.log('finished');
-						started.set(false);
-					}
-				}, 1000);
+
+		interval = setInterval(() => {
+			console.log('started');
+			currentPeriod.set($currentPeriod + 1); // increment current period
+			isRest.set(false);
+            console.log('period: ', $currentPeriod);
+			if ($currentPeriod === 5) {
+				console.log('finished');
+				started.set(false);
+                clearInterval(interval); // stops loop
 			}
-			if (!isRest) {
-				// if !isRest, incrememnt currentPeriod and set interval to 3s
-				interval = setTimeout(() => {
-					currentPeriod.set($currentPeriod + 1); // increment current period
-					isRest.set(true);
-					console.log('work');
-					console.log('period: ', $currentPeriod);
-				}, 3000);
-			}
-		}
+		}, 1000);
 	}
 
 	// function to update started store
