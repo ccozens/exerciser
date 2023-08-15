ed945119af183b7c45f14567bd8f65b5bfafe4b4
ccozens <ccozens@gmail.com>
2023-08-15 14:10:00 +0100

    tweened timer working well, having issues setting countdown

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,31 +1,53 @@
 <!-- script -->
 <script lang="ts">
-	import { currentPeriod, isRest, started, time } from '$lib/stores';
+	import { currentPeriod, isRest, started } from '$lib/stores';
 	import Start from '$lib/components/Start.svelte';
 	import Stop from '$lib/components/Stop.svelte';
+    import { reset } from '$lib/functions/Reset';
+    import { tick } from 'svelte';
+    import { tweened } from 'svelte/motion';
 
-    let interval ;
-    let rest = 1;
-    let work = 3;
-	// async function to start a timer
-	function testTimer() {
-		// abort if not started
+    let interval: number ; // MDN: "The returned intervalID is a numeric, non-zero value which identifies the timer created by the call to setInterval(); this value can be passed to WindowOrWorkerGlobalScope.clearInterval() to cancel the timeout."
+    let rest: number = 2;
+    let work: number = 5;
+    let numberOfPeriods: number = 5;
+
+    const workMs = work * 1000;
+    const restMs = rest * 1000;
+
+    // run workout using tweened
+    async function tweenedWorkout (rest: number, work: number, numberOfPeriods: number) {
+        // abort if not started
         if (!isStarted) return;
+        for (let i = 0; i < numberOfPeriods; i++) {
+        // if !isRest, increment currentPeriod and set tweened time to work * 100
+        if (!$isRest) {
+            currentPeriod.set($currentPeriod + 1);
+            await time.set(workMs, { duration: workMs });
+            time.set(0, {duration: 0});
+            toggleRest();
+            console.log('working', $currentPeriod, $isRest);
+        }
+        // if isRest, set tweened time to rest * 100
+        if ($isRest) {
+            await time.set(restMs, { duration: restMs });
+            time.set(0, {duration: 0});
+            toggleRest();
+            console.log('resting', $currentPeriod, $isRest);
+        }}
+        // run reset when exit loop:
+        reset();
+     /*    // if currentPeriod === numberOfPeriods, reset
+        if ($currentPeriod === numberOfPeriods) {
+            reset();
+        } */
+    }
 
-
-		interval = setInterval(() => {
-			console.log('started');
-			currentPeriod.set($currentPeriod + 1); // increment current period
-			isRest.set(false);
-            console.log('period: ', $currentPeriod);
-			if ($currentPeriod === 5) {
-				console.log('finished');
-				started.set(false);
-                clearInterval(interval); // stops loop
-			}
-		}, $isRest? rest * 1000 : work * 1000);
-	}
-
+    // toggle isRest
+    function toggleRest() {
+        isRest.set(!$isRest);
+        // $isRest === "Work" ? isRest.set("Rest") : isRest.set("Work");
+    }
 	// function to update started store
 	function setStarted() {
 		started.set(true);
@@ -33,11 +55,27 @@
 	$: isStarted = $started;
 	// run timer if started
 	$: if (isStarted) {
-		testTimer();
+		tweenedWorkout(rest, work, numberOfPeriods);
 	}
+
+      // use tweened to create timer with rest and work intervals, depending on isRest
+      const time = tweened(isRest? restMs : workMs);
+
+      // convert time to seconds
+      $: timeInSeconds = Math.round($time / 1000); // TODO should be *1000 but shortened so dev quicker
+      // convert to countdown
+        $: countdown = isRest ? rest - timeInSeconds : work - timeInSeconds;
 </script>
 
 <!-- html -->
+
+    <div>Current Period: {$currentPeriod}</div>
+        <div>Time Remaining: {timeInSeconds}</div>
+        <div>Countdown: {countdown}</div>
+    <div>Is Rest: {$isRest ? 'true' : 'false'}</div>
+    <div>Started: {$started}</div>
+
+
 <Start on:startSession={setStarted} />
 emits startSession on click
 <Stop />
