d00502ae25b29d42e2c387612cb9fdaaf3fbfcb7
ccozens <ccozens@gmail.com>
2023-08-23 15:36:04 +0100

    updated transition duration in ProgressBar to use CSS variable equal to tweened duration, so duration takes as long as tween

--- src/lib/components/ProgressBar.svelte
+++ src/lib/components/ProgressBar.svelte
@@ -4,18 +4,19 @@
 	import { tweened } from "svelte/motion";
 
 	export let tween: Tweened<number> = tweened(0, { duration: 0 }) ;
-	export let tweenedDuration: number = 1;
+	export let tweenedDuration: number = 0;
 </script>
 
 
 <div class="progress-wrapper">
-	<div class="progress-bar" style="height: {$tween/tweenedDuration}"></div>
+	<div class="progress-bar" style="height: {$tween/tweenedDuration*100}%; --tweenedDuration"></div>
 	{$tween}
 	{tweenedDuration}
 	{$tween/tweenedDuration}
 </div>
 
 <style>
+
 .progress-wrapper {
 		width: 20vw;
 		height: 20vh;
@@ -25,7 +26,10 @@
 	}
 	.progress-bar {
 		border-radius: 10px;
-		transition: height 1s linear;
+		/* transition: height 1s linear; */
+		transition-property: height;
+		transition-duration: var(--tweenedDuration)s;
+		transition-timing-function: linear;
 		background: linear-gradient(
 			rgba(2, 0, 36, 1) 0%,
 			rgba(9, 9, 121, 1) 35%,
