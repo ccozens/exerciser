2632b73312181594459aa782a54a66d1555a226e
ccozens <ccozens@gmail.com>
2023-09-22 10:11:33 +0100

    updated ScreenWakeLock.svelte to have separate requestWakeLock function that checks for wakelock before starting, and added onVisibilityChange to ensure wakelock maintained if visibility changes

--- src/lib/components/ScreenWakeLock.svelte
+++ src/lib/components/ScreenWakeLock.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
+
 	let wakeLock: WakeLockSentinel | null = null;
 
-	onMount(() => {
-		if (browser && 'wakeLock' in navigator) {
-			navigator.wakeLock.request('screen').then((wl) => {
-				wakeLock = wl;
-			});
+	async function requestWakeLock() {
+		// check if wake lock is supported by the browser
+		if ('wakeLock' in navigator) {
+			try {
+				// request a wake lock
+				wakeLock = await navigator.wakeLock.request('screen');
+				wakeLock.addEventListener('release', () => {});
+			} catch (err) {
+				console.error(`${err.name}, ${err.message}`);
+			}
+		} else {
+			console.log('Wake lock is not supported by this browser');
 		}
-	});
+	}
+
+	// re-request wakelock on visibility change
+	async function onVisibilityChange() {
+		if (wakeLock !== null && document.visibilityState === 'visible') {
+			await requestWakeLock();
+		}
+	}
 
 	function releaseWakeLock() {
 		if (wakeLock) {
@@ -17,6 +32,20 @@
 			wakeLock = null;
 		}
 	}
+
+	// call wake lock onmount
+	onMount(() => {
+		if (browser && 'wakeLock' in navigator) {
+			requestWakeLock();
+			document.addEventListener('visibilitychange', onVisibilityChange);
+		}
+	});
+
+	// release wake lock and remove event listener on destroy
+	onDestroy(() => {
+		releaseWakeLock();
+		document.removeEventListener('visibilitychange', onVisibilityChange);
+	});
 </script>
 
 {#if browser && 'wakeLock' in navigator}
